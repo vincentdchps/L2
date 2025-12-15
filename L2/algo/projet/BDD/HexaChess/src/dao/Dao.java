c3d476cde76334bd4cb83e2d3a7242b40007c41d
Vincehhh <104105898+Vincehhh@users.noreply.github.com>
2025-12-15 19:06:19 +0100

    Update Dao.java

--- L2/algo/projet/BDD/HexaChess/src/dao/Dao.java
+++ L2/algo/projet/BDD/HexaChess/src/dao/Dao.java
@@ -1,11 +1,41 @@
-package src.dao;
-import java.util.List;
-import java.util.Optional;
+package tp7.dao;
 
-public interface Dao<T> {
-    Optional<T> get(String id);
-    List<T> getAll();
-    void save(T t);
-    void update(T t, String[] params);
-    void delete(T t);
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public abstract class DAO<T> {
+	protected Connection connect;
+	protected Statement stmt;
+
+	public DAO() {
+		open();
+	}
+
+	public void open() {
+		try {
+			connect = SingleConnection.getInstance();
+			stmt = connect.createStatement(
+				ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+
+		} catch (Exception e) {
+			System.out.println(" === ERREUR OPEN DAO === ");
+			e.printStackTrace();
+		}
+	}
+
+	public void close() {
+		try {
+			SingleConnection.close();
+		} catch (Exception e) {
+			System.out.println(" === ERREUR CLOSE DAO === ");
+			e.printStackTrace();
+		}
+	}
+
+	
+	public abstract T create(T obj);
+	public abstract T update(T obj);
+	public abstract void delete(T obj);
 }
