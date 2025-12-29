a9cc321b642a087e1e78715e0fb7e0b961fe6e51
Vincehhh <104105898+Vincehhh@users.noreply.github.com>
2025-12-29 13:07:46 +0100

    Create ex1.c

---
+++ L2/ProgSyst/tp/tp1/ex1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int a = 2;
+    int b = 3;
+    int c = 11;
+
+    double moyenne = (a + b + c) / 3.0;
+    printf("La moyenne est : %.2f\n", moyenne);
+
+    return 0;
+}
