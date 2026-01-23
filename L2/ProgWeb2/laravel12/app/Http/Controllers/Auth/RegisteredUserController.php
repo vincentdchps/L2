c7ceb8147b32bce9ee1a6d6e6c7ac54dfd80fd1e
Vincehhh <bomberbrite04@gmail.com>
2026-01-23 12:08:50 +0100

    add-firstname

--- L2/ProgWeb2/laravel12/app/Http/Controllers/Auth/RegisteredUserController.php
+++ L2/ProgWeb2/laravel12/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -30,12 +30,14 @@ class RegisteredUserController extends Controller
     public function store(Request $request): RedirectResponse
     {
         $request->validate([
+            'first name' => ['required', 'string', 'max:255'],
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
         $user = User::create([
+            'first_name' => $request->first_name,
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
