ad0921b495dd42cdb7563ad6d503f1d8e1cee482
Vincehhh <bomberbrite04@gmail.com>
2026-02-06 18:13:09 +0100

    add-test

--- L2/ProgWeb2/laravel122/tests/Feature/Auth/RegistrationTest.php
+++ L2/ProgWeb2/laravel122/tests/Feature/Auth/RegistrationTest.php
@@ -20,6 +20,7 @@ class RegistrationTest extends TestCase
     {
         $response = $this->post('/register', [
             'name' => 'Test User',
+            'lastname' => 'Test',
             'email' => 'test@example.com',
             'password' => 'password',
             'password_confirmation' => 'password',
