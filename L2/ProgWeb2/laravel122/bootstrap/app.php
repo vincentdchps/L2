9e6d52783f2b1c0421c95e5c8adf21f06ad2b3d4
Vincehhh <bomberbrite04@gmail.com>
2026-02-13 13:11:07 +0100

    add-midleware-user

--- L2/ProgWeb2/laravel122/bootstrap/app.php
+++ L2/ProgWeb2/laravel122/bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\AdminMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -10,9 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
-        //
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->group('is_admin', [AdminMiddleware::class]);
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
+    ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
