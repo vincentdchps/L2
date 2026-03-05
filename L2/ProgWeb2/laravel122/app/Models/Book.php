092427617cdb75753088e441047b4f6d43b9b166
Vincehhh <bomberbrite04@gmail.com>
2026-03-05 21:27:51 +0100

    add function attribute price

--- L2/ProgWeb2/laravel122/app/Models/Book.php
+++ L2/ProgWeb2/laravel122/app/Models/Book.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+
 
 class Book extends Model
 {
@@ -16,6 +18,15 @@ class Book extends Model
     public function author(): BelongsTo {
         return $this->belongsTo(Author::class);
     }  
+
+
+protected function price(): Attribute
+{
+    return Attribute::make(
+        get: fn (int $value) => $value / 100,
+        set: fn (float $value) => $value * 100,
+    );
+}
 }
 
 
