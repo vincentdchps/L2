bc4178137e140ea50f353a34617c4af40ad795d9
Vincehhh <bomberbrite04@gmail.com>
2026-03-21 14:39:28 +0100

    Back book.php

---
+++ L2/ProgWeb2/laravel122/app/Http/Requests/Back/UpdateBookRequest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Requests\Back;
+
+use App\Http\Requests\Back\StoreBookRequest;
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateBookRequest extends StoreBookRequest
+{
+
+}
