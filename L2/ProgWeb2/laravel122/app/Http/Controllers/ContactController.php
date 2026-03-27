eeca24b530be86f0afd8f1ec65fba06825edd3f9
Vincehhh <bomberbrite04@gmail.com>
2026-03-27 12:03:08 +0100

    Contact

---
+++ L2/ProgWeb2/laravel122/app/Http/Controllers/ContactController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Mail\ContactMessage;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Gate;
+
+class ContactController extends Controller
+{
+    public function show()
+    {
+        // Seul un 'user' classique peut voir le formulaire
+        if (auth()->user()->role !== 'user') {
+            abort(403, 'Accès réservé aux utilisateurs standards.');
+        }
+        return view('front.contact'); // La vue avec ton formulaire HTML (titre, message)
+    }
+
+    public function submit(Request $request)
+{
+    if (auth()->user()->role !== 'user') {
+        abort(403);
+    }
+
+    $validated = $request->validate([
+        'title' => 'required|string|max:255',
+        'message' => 'required|string',
+    ]);
+
+    $contact = [
+        'name' => auth()->user()->firstname . ' ' . auth()->user()->name,
+        'title' => $validated['title'],
+        'content' => $validated['message'],
+    ];
+
+    $suUsers = User::where('role', 'su')->get();
+
+    foreach ($suUsers as $su) {
+        Mail::to($su->email)->send(new ContactMessage($contact));
+    }
+
+    return redirect()->back()->with('success', 'Votre message a bien été envoyé aux administrateurs.');
+    }
+}
+
